dfdde81f5be63903a0b8df0927203231474a7df1
Harrison <hmseaborn@gmail.com>
2020-03-13 09:13:45 -0700

    add DarkMode hook and persist to localstorage

---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkMode", true);
+    useEffect(() => {
+        darkMode ? document.body.classList.add("dark-mode") : document.body.classList.remove('dark-mode')
+    },[darkMode])
+    return [darkMode, setDarkMode]
+}
