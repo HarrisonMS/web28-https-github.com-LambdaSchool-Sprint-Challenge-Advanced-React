a57baacf5937bad0a545bb363770608396583595
Harrison <hmseaborn@gmail.com>
2020-03-13 08:49:20 -0700

    get request made to localhost successful

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
       users: []
     }
   }
-  rednder(){
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`)
+    .then((res) => {
+      this.setState({
+        users:res.data
+      })
+      console.log("users data", res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  render(){
     return (
       <div className="App">
       <h1>Women's World Cup players</h1>
