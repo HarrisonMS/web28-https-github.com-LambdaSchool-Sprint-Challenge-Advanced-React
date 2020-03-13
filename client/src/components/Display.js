a43a136734f6ae3ef185009884ec8ef6a759413e
Harrison <hmseaborn@gmail.com>
2020-03-13 08:52:54 -0700

    HotFix playercard location incorrect

--- client/src/components/Display.js
+++
@@ -1,9 +0,0 @@
-export const PlayerCard = props => {
-    return (
-        <div>
-            <h2>Name: {props.name}</h2>
-            <p>Country: {props.country}</p>
-            <p>Google searches:{props.searches}</p>
-        </div>
-    )
-}
